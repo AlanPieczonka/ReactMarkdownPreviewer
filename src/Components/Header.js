04d9279797c8c05490aed95bb6e1f42487efc910
Alan Pieczonka <alanpieczonka@gmail.com>
2017-09-02 20:12:20 +0200

    UI changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import customHeaderCSS from './../css/ComponentsStyles/customHeader.css';
 import reactLogo from './../css/img/react-logo.png';
-
+import bootstrapLogo from './../css/img/bootstrap-logo.png';
 export const Header = () => {
   return (
     <header>
       <h1 className="header__h1 text-center">Markdown Previewer</h1>
-      <h2 className="header__h2 text-center">
-        Made with <span className="span span--react">React</span>
-      </h2>
-      <img src={reactLogo} className="img--reactlogo" alt="React Icon" />
+      <div className="div--logo">
+        <a href="https://facebook.github.io/react/" target="_blank"><img src={reactLogo} className="img--technologylogo" alt="React Icon" /></a>
+        <a href="https://v4-alpha.getbootstrap.com/" target="_blank"><img src={bootstrapLogo} className="img--technologylogo" alt="React Icon" /></a>
+      </div>
     </header>
   );
 };
