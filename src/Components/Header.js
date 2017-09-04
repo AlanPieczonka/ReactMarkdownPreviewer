1d98d571ffe36483e90e36d3886cbc77ab0f5ec2
Alan Pieczonka <alanpieczonka@gmail.com>
2017-09-04 17:58:06 +0200

    Finished Header.test.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,8 +8,8 @@ export const Header = () => {
     <header>
       <h1 className="header__h1 text-center">Markdown Previewer</h1>
       <div className="div--logo">
-        <a href="https://facebook.github.io/react/" target="_blank"><img src={reactLogo} className="img--technologylogo" alt="React Icon" /></a>
-        <a href="https://v4-alpha.getbootstrap.com/" target="_blank"><img src={bootstrapLogo} className="img--technologylogo" alt="React Icon" /></a>
+        <a href="https://facebook.github.io/react/" target="_blank"><img src={reactLogo} className="img--technologylogo img--technologylogo--reactlogo" alt="React Icon" /></a>
+        <a href="https://v4-alpha.getbootstrap.com/" target="_blank"><img src={bootstrapLogo} className="img--technologylogo img--technologylogo--bootstraplogo" alt="React Icon" /></a>
       </div>
     </header>
   );
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { expect, context } from 'chai';
-import {mount, render, shallow} from 'enzyme';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 
 import { Header } from './../Components/Header';
 
 describe('Header Component', () => {
-    it('renders without crashing', () => {
-        shallow(<Header />);
-    });
+  it('renders without crashing', () => {
+    shallow(<Header />);
+  });
+
+  it('renders React logo', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('.img--technologylogo--reactlogo')).to.have.length(1);
+  });
+
+  it('renders Bootstrap logo', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('.img--technologylogo--bootstraplogo')).to.have.length(1);
+  });
+
+  it('renders H1 ', () => {
+    const wrapper = shallow(<Header/>);
+    expect(wrapper.find('h1'));
+  });
+
+  it(' H1s text is Markdown Previewer', () => {
+    const wrapper = shallow(<Header/>);
+    expect(wrapper.find('h1').text()).to.equal('Markdown Previewer');
+  });
+
 });
