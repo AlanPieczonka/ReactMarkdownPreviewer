f711db7aa079f52801159f12a8025fd380d36c9b
Alan Pieczonka <alanpieczonka@gmail.com>
2017-09-04 19:10:53 +0200

    Finished footer tests

--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -10,4 +10,29 @@ describe('Footer Component', () => {
     shallow(<Footer />);
   });
 
+  it('renders h4', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('h4')).to.have.length(1);
+  });
+
+  it('s h4 text is Created by Alan Pieczonka, 2017', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('h4').text()).to.equal(
+      'Created by Alan Pieczonka, 2017'
+    );
+  });
+
+  it('renders Github logo', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('.img--githublogo')).to.have.length(1);
+  });
+
+  it('a href element has proper link to repository', () => {
+    const wrapper = mount(<Footer />);
+    expect(
+      wrapper.find({
+        href: 'https://github.com/AlanPieczonka/ReactMarkdownPreviewer'
+      })
+    ).to.have.length(1);
+  });
 });
