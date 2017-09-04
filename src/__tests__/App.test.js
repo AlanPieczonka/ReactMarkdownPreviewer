8b6e6b13aa9462df3e5e271dfbbd53cff2485923
Alan Pieczonka <alanpieczonka@gmail.com>
2017-09-04 17:57:23 +0200

    Deleted unnecessary imports in App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,38 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { expect, context } from 'chai';
-import {mount, render, shallow} from 'enzyme';
-
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 
 import App from './../Components/App';
 import { Header } from './../Components/Header';
-import  Main  from './../Components/Main';
+import Main from './../Components/Main';
 import { Footer } from './../Components/Footer';
 
 describe('App Component', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
 
-    it('renders without crashing', () => {
-        shallow(<App />);
-    });
+  it('renders 3 children component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.children()).to.have.length(3);
+  });
 
-    it('renders 3 children component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.children()).to.have.length(3);
-    });
+  it('renders Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header)).to.have.length(1);
+  });
 
-    it('renders Header component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(Header)).to.have.length(1);
-    });
+  it('renders Main component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Main)).to.have.length(1);
+  });
 
-    it('renders Main component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(Main)).to.have.length(1);     
-    });
-
-    it('renders Footer component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(Footer)).to.have.length(1);     
-    });
-
+  it('renders Footer component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer)).to.have.length(1);
+  });
 });
